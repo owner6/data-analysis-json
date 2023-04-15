96f2a2007e1f04f660bff5be231dd96e5ef516ec
owner6 <owner1147@gmail.com>
2023-04-15 23:30:43 +0300

    weight and height (max, min, average)

--- data-analysis.js
+++ data-analysis.js
@@ -1,8 +1,79 @@
-	fetch('https://dummyjson.com/users')
-  .then(res => res.json())
-  .then(users => {
-		users.forEach(user => {
-			console.log(user.email)
+//вес
+let url = 'https://dummyjson.com/users';
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+		let maxWeight = 0
+    data.users.forEach(function(user) {
+			if(user.weight > maxWeight)  {
+				maxWeight = user.weight
+			}
+    })
+		console.log('Максимлаьный вес: ', maxWeight)
+  })
+
+fetch(url)
+	.then(response => response.json())
+	.then(data => {
+		let minWeight = Number.MAX_VALUE;
+		data.users.forEach(function(user) {
+			if (user.weight < minWeight) {
+				minWeight = user.weight;
+			}
 		});
+		console.log('Минимальный вес: ', minWeight);
+	});
+
+fetch(url)
+	.then(response => response.json())
+	.then(data => {
+		let totalWeight = 0
+		data.users.forEach(function(user) {
+			totalWeight += user.weight
+		})
+		let averageWeight = totalWeight / data.users.length
+		console.log(averageWeight)
+	});
+
+//рост
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+		let maxHeight = 0
+    data.users.forEach(function(user) {
+			if(user.height > maxHeight)  {
+				maxHeight = user.height
+			}
+    })
+		console.log('Максимальный рост: ', maxHeight)
+  })
+
+fetch(url)
+	.then(response => response.json())
+	.then(data => {
+		let minHeight = Number.MAX_VALUE;
+		data.users.forEach(function(user) {
+			if (user.height < minHeight) {
+				minHeight = user.height;
+			}
+		})
+		console.log('Минимальный вес: ', minHeight);
+	})
+
+	fetch(url)
+	.then(response => response.json())
+	.then(data => {
+		let totalHeight = 0
+		data.users.forEach(function(user) {
+			totalHeight += user.height
+		})
+		let averageHeight = totalHeight / data.users.length
+		console.log('Средний рост', averageHeight)
 	})
 	
+
+
+
+
+
+
