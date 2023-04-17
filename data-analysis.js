3c4047ef37b76bec38f2ae18a99127e1c17d0d64
owner6 <owner1147@gmail.com>
2023-04-17 21:43:47 +0300

    find max and min weight

--- data-analysis.js
+++ data-analysis.js
@@ -1,91 +1,41 @@
-const url = 'https://dummyjson.com/users';
-getData(url)
-  .then(users => {
-    const results = processData(users);
-    displayResults(results);
-  })
+const url = 'https://dummyjson.com/users'
 
-  function getData(url) {
-    return fetch(url)
-      .then(response => response.json())
-      .then(data => {
-      return data.users;
-      })
-    }
+function getData(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const users = data.users;
+      const maxWeight = findMaxWeight(users)
+      const minWeight = findMinWeight(users)
+      return { 
+				users, 
+				maxWeight,
+				minWeight 
+			}
+    })
+}
 
-function processData(users) {
-  let maxWeight = 0
-  let minWeight = Number.MAX_VALUE
-  let maxHeight = 0
-  let minHeight = Number.MAX_VALUE
-  let maxWeightUser = null
-  let minWeightUser = null
-  let maxHeightUser = null
-  let minHeightUser = null
-  let totalWeight = 0
-  let totalHeight = 0
-  let malesHeight = []
-  let malesWeight = []
-  
-  users.forEach(function(user) {
-    if (user.weight > maxWeight) {
-      maxWeight = user.weight;
-      maxWeightUser = user;
+function findMaxWeight(users) {
+  let maxWeight = 0;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].weight > maxWeight) {
+       maxWeight = users[i].weight
     }
-    if (user.weight < minWeight) {
-    minWeight = user.weight;
-      minWeightUser = user;
-    }
-  
-    totalWeight += user.weight
-    totalHeight += user.height
-
-    if (user.height > maxHeight)  {
-      maxHeight = user.height;
-      maxHeightUser = user;
-    }
-    if (user.height < minHeight)  {
-      minHeight = user.height;
-      minHeightUser = user;
-    }
-    if (user.gender === "male") {
-      malesHeight.push(user)
-    }
-    if (user.gender === "male") {
-      malesWeight.push(user)
-    }
-   
-  })
-
-  const averageWeight = totalWeight / users.length
-  const averageHeight = totalHeight / users.length
-
-  const tallestMale = malesHeight.filter(user => user.gender === "male").reduce((prev, current) => (prev.height > current.height) ? prev : current);
-  const tallestMaleWeight = malesWeight.filter(user => user.gender === "male").reduce((prev, current) => (prev.weight > current.weight) ? prev : current);
-  
-  return {
-    maxWeight,
-    minWeight,
-    maxHeight,
-    minHeight,
-    maxWeightUser,
-    minWeightUser,
-    maxHeightUser,
-    minHeightUser,
-    averageWeight,
-    averageHeight,
-    tallestMale,
-    tallestMaleWeight
   }
+  return maxWeight
 }
 
-function displayResults(results) {
-  document.getElementById("outputMaxWeight").textContent = 'Имя пользователя с максимальным весом: ' + results.maxWeightUser.firstName + ' Максимальный вес: ' + results.maxWeight
-  document.getElementById("outputMinWeight").textContent = 'Имя пользователя с минимальным весом: ' + results.minWeightUser.firstName + ' Минимальный вес: ' + results.minWeight
-  document.getElementById("outputMaxHeight").textContent = 'Имя пользователя с максимальным ростом: ' + results.maxHeightUser.firstName + ' Максимальный рост: ' + results.maxHeight
-  document.getElementById("outputMinHeight").textContent = 'Имя пользователя с минимальным ростом: ' + results.minHeightUser.firstName + ' Минимальный рост: ' + results.minHeight
-  document.getElementById("outputAverageWeight").textContent = 'Средний вес: ' + results.averageWeight;
-  document.getElementById("outputAverageHeight").textContent = 'Средний рост: ' + results.averageHeight;
-  document.getElementById("outputTallestMale").textContent = 'Имя самого высокого мужчины: ' + results.tallestMale.firstName
-  document.getElementById("outputTallestMaleWeight").textContent = 'Имя самого тяжелого мужчины: ' + results.tallestMaleWeight.firstName
+function findMinWeight(users) {
+  let minWeight = Number.MAX_VALUE
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].weight < minWeight) {
+       minWeight = users[i].weight 
+    }
+  }
+  return minWeight
 }
+
+getData(url).then(data => {
+  console.log(data.maxWeight)
+  console.log(data.minWeight)
+})
